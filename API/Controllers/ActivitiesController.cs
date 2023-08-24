d9aaf74b5f28cb27178e22de14a009f744fb99a1
Fredrik Widén <fwiden@gmail.com>
2023-08-24 08:14:09 +0200

    test

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -13,7 +13,7 @@ namespace API.Controllers
             _context = context;
         }
 
-        [HttpGet] //api/activities
+        [HttpGet] //api/activities testar
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
             return await _context.Activities.ToListAsync();
